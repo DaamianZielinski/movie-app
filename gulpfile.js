0a4d8315977774a82ae8b1890591bf9463cda540
Zdamian <damianzielin.js@gmail.com>
2017-01-31 18:45:22 +0100

    angularjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,72 @@
 var
-	gulp = require("gulp"),
-	sass = require("gulp-sass"),
-	path = require("path");
+    gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    path = require('path'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat');
 
 var paths = {
-	appScss: ["app/scss/**/*.scss"]
+    appScss: [
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'app/scss/**/*.scss'
+    ],
+    appFonts: ['app/fonts/**/*'],
+    appJs: ['app/js/**/*.js']
 };
 
-gulp.task("sass", function () {
-	return gulp.src(paths.appScss)
-		.pipe(sass().on("error", sass.logError))
-		.pipe(gulp.dest(function (file) {
-			return path.join(file.base, "..", "css");
-		}))
-		pipe(gulp.dest("app/css"))
+gulp.task('sass', function() {
+    return gulp.src(paths.appScss)
+        .pipe(concat('styles.css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(function(file) {
+            return path.join(file.base, '..', 'css');
+        }))
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task("watch-sass", ["sass"], function() {
-	gulp.watch(paths.appScss, ["sass"])
+gulp.task('fonts', function () {
+    return gulp.src(paths.appFonts)
+      .pipe(gulp.dest(function (file) {
+          return path.join(file.base, '..', 'fonts');
+      }))
+      .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('build.dev', ['fonts', 'sass'], function(){
+    return gulp
+        .src([
+            // dependencies
+            'node_modules/angular/angular.min.js',
+
+            // project source
+            'app/js/**/*.js',
+        ])
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'));
+        // include dist/app.js in index.html
+});
+
+gulp.task('build.prod', ['fonts', 'sass'], function(){
+    return gulp
+        .src([
+            // dependencies
+            'node_modules/angular/angular.min.js',
+
+            // project source
+            'app/js/**/*.js',
+        ])
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'));
+        // include dist/app.js in index.html
+});
+
+gulp.task('watch.dev', ['build.dev'], function() {
+    gulp.watch(paths.appScss, ['sass']);
+    gulp.watch(paths.appJs, ['build.dev']);
 });
